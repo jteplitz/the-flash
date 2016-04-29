602af86f9e8f0704158d6adfd7b8f5abe817026e
Jason Teplitz <jteplitz602@gmail.com>
2016-04-28 17:57:02 -0700

    add over/underuse detector

--- datagrump/controller.cc
+++ datagrump/controller.cc
@@ -5,14 +5,27 @@
 
 // packet length in bits
 #define PACKET_LENGTH 12000
+#define ALPHA 0.2
+// burst time in milliseconds
+// used to group together packet bursts
+#define BURST_TIME 5
+
+// over/underuse change threshold
+#define THRESHOLD 0.5
+//#define OVERUSE_TIME 10
 
 using namespace std;
 
 /* Default constructor */
 Controller::Controller( const bool debug )
   : debug_( debug ),
-    prev_packet_({0, 0}),
-    send_rate_(1000000)
+    prev_packet_({0, 0, 0}),
+    send_rate_(1000000),
+    packet_send_rates_(),
+    curr_gradient_(0),
+    prev_rtt_(0),
+    prev_measured_time_(0),
+    congestion_state_(Underuse)
 {}
 
 /* Get current window size, in datagrams */
@@ -36,6 +49,7 @@ void Controller::datagram_was_sent( const uint64_t sequence_number,
                                     /* in milliseconds */
 {
   /* Default: take no action */
+  packet_send_rates_[sequence_number] = send_rate_;
 
   if ( debug_ ) {
     cerr << "At time " << send_timestamp
@@ -53,30 +67,58 @@ void Controller::ack_received( const uint64_t sequence_number_acked,
 			       const uint64_t timestamp_ack_received )
                                /* when the ack was received (by sender) */
 {
-  /* Default: take no action */
-  if (prev_packet_.send_time == 0 && prev_packet_.receive_time == 0){
-    prev_packet_ = {send_timestamp_acked, recv_timestamp_acked};
+  PacketData curr_packet = {send_timestamp_acked, recv_timestamp_acked, timestamp_ack_received};
+  uint64_t rtt = timestamp_ack_received - send_timestamp_acked;
+  if ((prev_packet_.send_time == 0 && prev_packet_.receive_time == 0)){
+    prev_packet_ = curr_packet;
+    prev_measured_time_ = timestamp_ack_received;
+    prev_rtt_ = rtt;
     return;
   }
 
-  PacketData curr_packet = {send_timestamp_acked, recv_timestamp_acked};
   auto relative_iat = compute_relative_iat_(curr_packet, prev_packet_);
-  prev_packet_ = curr_packet;
-
-  auto pacing_delay = send_rate_to_pacing_delay_(send_rate_);
-  auto old_send_rate = send_rate_;
-  if (pacing_delay == 0 && relative_iat < 0) {
-    pacing_delay = (double) relative_iat / 1000;
-    send_rate_ = pacing_delay_to_send_rate_(pacing_delay);
+  auto iat = curr_packet.receive_time - prev_packet_.receive_time;
+  if (iat < BURST_TIME && relative_iat < 0) {
+    prev_packet_ = curr_packet;
+    return;
   }
-  if ((double) pacing_delay + (double) relative_iat / 1000 < 0) {
-    send_rate_ = 0;
+
+  if (iat < rtt) {
+    rtt -= iat;
   } else {
-    pacing_delay += (double) relative_iat / 1000;
-    send_rate_ = pacing_delay_to_send_rate_(pacing_delay);
+    cerr << "Huge IAT!!" << endl;
+    prev_packet_ = curr_packet;
+    // TODO: Trigger overuse?
+    return;
   }
-  cout << old_send_rate << ',' << relative_iat << ',' << send_rate_ << endl;
 
+  auto delta = timestamp_ack_received - prev_measured_time_;
+  if (delta == 0) {
+    prev_packet_ = curr_packet;
+    return;
+  }
+  auto gradient = gradient_(rtt, prev_rtt_, curr_gradient_, delta);
+  //cout << timestamp_ack_received << ',' << gradient << endl;
+  cout << timestamp_ack_received << ',';
+
+  if (gradient > THRESHOLD) {
+    // TODO: Fancier triggering of overuse?
+    congestion_state_ = Overuse;
+    cout << -1;
+  } else if (gradient < -1 * THRESHOLD){
+    congestion_state_ = Underuse;
+    cout << 1;
+  } else {
+    congestion_state_ = Normal;
+    cout << 0;
+  }
+
+  cout << endl;
+
+  prev_packet_ = curr_packet;
+  prev_measured_time_ = timestamp_ack_received;
+
+  packet_send_rates_.erase(sequence_number_acked);
   if ( debug_ ) {
     cerr << "At time " << timestamp_ack_received
 	 << " received ack for datagram " << sequence_number_acked
@@ -112,6 +154,15 @@ int Controller::compute_relative_iat_(PacketData packet1, PacketData packet2)
   return receive_time_diff - send_time_diff;
 }
 
+double Controller::gradient_(const uint64_t curr, uint64_t &prev, double &diff,
+                             uint64_t delta)
+{
+  double new_diff = ((double) curr - (double) prev) / delta;
+  prev = curr;
+  diff = (1.0 - ALPHA) * diff + ALPHA * new_diff;
+  return diff;
+}
+
 /* How long to wait (in milliseconds) if there are no acks
    before sending one more datagram */
 unsigned int Controller::timeout_ms( void )
@@ -123,5 +174,5 @@ unsigned int Controller::send_rate( void )
 {
   // computed send rate or 10 packets per second
   //return max((uint64_t) send_rate_, (uint64_t) PACKET_LENGTH * 10);
-  return 3000000;
+  return 4000000;
 }
--- datagrump/controller.hh
+++ datagrump/controller.hh
@@ -2,13 +2,19 @@
 #define CONTROLLER_HH
 
 #include <cstdint>
+#include <map>
 
 /* Congestion controller interface */
 
 typedef struct PacketData {
   uint64_t send_time;
   uint64_t receive_time;
+  uint64_t measured_time;
 } PacketData;
+
+typedef enum CongestionState {
+  Overuse, Underuse, Normal
+} CongestionState;
 
 class Controller
 {
@@ -17,11 +23,19 @@ private:
 
   PacketData prev_packet_;
   unsigned int send_rate_;
+  std::map<int, unsigned int> packet_send_rates_;
+  double curr_gradient_;
+  uint64_t prev_rtt_;
+  uint64_t prev_measured_time_;
+  CongestionState congestion_state_;
 
   int compute_relative_iat_(PacketData packet1, PacketData packet2);
 
   double send_rate_to_pacing_delay_(unsigned int send_rate);
   unsigned int pacing_delay_to_send_rate_(double pacing_delay);
+
+  double gradient_(const uint64_t curr, uint64_t &prev, double &diff,
+                               uint64_t delta);
 
 public:
   /* Public interface for the congestion controller */
